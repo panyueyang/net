2e2a6d78a9cd6816b7fe044e84a9535103faa5ae
panyueyang <57025908+panyueyang@users.noreply.github.com>
2020-06-11 18:15:18 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,4 +1,11 @@
 function locat(file_link)
 {
-  location = file_link;
+  if(getQueryString("login"=="true")
+  location = file_link+"?login=true";
+     else location = file_link;
 }
+function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    }
