e591e968b94e3b4fabbc46fb97d25f59a7144e45
panyueyang <57025908+panyueyang@users.noreply.github.com>
2020-07-11 16:35:44 +0800

    重大Debug！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！（此处省略无数个！）
    改正诸如panyueyang.ml/e.html?e=1?login=true
    改为panyueyang.ml/e.html?e=1&login=true
    不会出现跳转后链接两个？的问题了
    妈妈再也不用担心Bug了！！！

--- location.js
+++ location.js
@@ -1,10 +1,10 @@
-function locat(file_link)
+function locat(add,file_link)
 {
   if(getQueryString("login"))
-  location = file_link+"?login=true";
+  if(add)location = file_link+"?login=true";else location=file_link+"&login=true"
      else location = file_link;
 }
-function getQueryString(name) {
+function getQueryString(add,name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
         var r = top.window.location.search.substr(1).match(reg);
         if (r != null) return unescape(r[2]); return null;
